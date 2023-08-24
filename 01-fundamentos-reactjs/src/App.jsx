d3818855a86605dcb32d940434f230093f361e23
EliasMedeirosDev <eliasmedeiros898@gmail.com>
2023-08-24 19:06:11 -0300

    Estrutura dos posts criada

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,8 @@
-import { Post } from './Post';
 import { Header } from './components/Header';
 import styles from './App.module.css';
 import './global.css';
 import { Sidebar } from './components/Sidebar';
+import {Post} from './components/Post'
 
 export function App() {
   return (
@@ -16,14 +16,9 @@ export function App() {
             <Sidebar />
             
             <main>
-              <Post 
-                author='Elias Medeiros'
-                content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, nemo blanditiis pariatur sequi illo tempora vero doloribus facere non corrupti, expedita aliquam explicabo sapiente. Ea cum esse adipisci fuga eligendi?'
-              />
-              <Post 
-                author='Anna Ellen'
-                content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, nemo blanditiis pariatur sequi illo tempora vero doloribus facere non corrupti, expedita aliquam explicabo sapiente. Ea cum esse adipisci fuga eligendi?'
-              />
+              
+              <Post />
+              
             </main>
         </div>
         
---
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -0,0 +1,36 @@
+import styles from './Post.module.css'
+export function Post(){
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/102063068?v=4" />
+                    <div className={styles.authorInfo}>
+                        <strong>Elias Medeiros</strong>
+                        <span>Junior Developer</span>
+
+
+
+                    </div>
+
+                </div>
+
+                <time dateTime="2023-08-24 18:58:59">
+                    Publicado há 1hr
+                </time>
+            </header>
+
+            <div className={styles.content}>
+            <p>Fala galeraa 👋</p>
+
+            <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+            </p>
+            
+            <p>👉 <a href="">jane.design/doctorcare</a></p>
+
+            <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+            </div>
+
+        </article>
+        )
+}
