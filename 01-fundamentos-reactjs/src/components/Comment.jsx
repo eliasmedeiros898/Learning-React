770d7d7f80de312ad7c95df807b6cf09d6e99586
EliasMedeirosDev <eliasmedeiros898@gmail.com>
2023-08-25 16:51:13 -0300

    Aperfeiçoamento do campo de comentários

---
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -0,0 +1,31 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+export function Comment(){
+    return(
+        <div className={styles.comment}>
+            <img src="https://avatars.githubusercontent.com/u/82901702?v=4"/>
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Alexandre Star</strong>
+                            <time dateTime="2023-08-24 18:58:59">
+                                Cerca de 1hr atrás
+                            </time>
+                        </div>
+                        <button title='Deletar comentário'>
+                            <Trash size={20}/>
+                        </button>
+                    </header>
+                    <p>Muito bom, Parabéns!</p>
+                </div>
+                <footer>
+                    <button>
+                        <ThumbsUp/>
+                        Aplaudir <span>20</span>
+                        </button> 
+                </footer>
+            </div>
+        </div>
+    )
+}
