bfe770d5ace965a20b456af10ede405d0d1ea0f3
EliasMedeirosDev <eliasmedeiros898@gmail.com>
2023-08-28 18:56:09 -0300

    Atualização dos posts

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -2,7 +2,43 @@ import { Header } from './components/Header';
 import styles from './App.module.css';
 import './global.css';
 import { Sidebar } from './components/Sidebar';
-import {Post} from './components/Post'
+import { Post } from './components/Post'
+
+const posts = [
+  {
+  id: 1,
+  author: {
+    avatarUrl: 'https://avatars.githubusercontent.com/u/102063068?v=4',
+    name: 'Elias Medeiros',
+    role: 'Desenvolvedor Junior'
+    },
+  content: [
+    {type: 'paragraph', text:'Fala galeraa 👋'},
+    {type: 'paragraph', text:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+    {type:'link',text:'jane.design/doctorcare'},
+
+  ],
+  publishedAt: new Date('2023-08-28 16:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/saulojustiniano1.png',
+      name: 'Saulo Justiniano',
+      role: 'Desenvolvedor Junior'
+      },
+    content: [
+      {type: 'paragraph', text:'Hello 👋'},
+      {type: 'paragraph', text:'Follow My channel! 🚀'},
+      {type:'link',text:'youtube.com/saulosclub'},
+  
+    ],
+    publishedAt: new Date('2023-08-28 17:00:00')
+    }
+
+
+]
+
 
 export function App() {
   return (
@@ -17,8 +53,15 @@ export function App() {
             
             <main>
               
-              <Post />
-              <Post />
+              {posts.map(post => {
+                return (
+                <Post
+                  author={post.author}
+                  content={post.content}
+                  publishedAt={post.publishedAt}
+
+                  />)
+              })}
               
             </main>
         </div>
