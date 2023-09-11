e2b0a67fda2ac6e349f2892a9aea624b8959fcbb
EliasMedeirosDev <eliasmedeiros898@gmail.com>
2023-09-11 15:25:35 -0300

    Adicionadas as rotas

--- 02-ignite-timer/src/App.tsx
+++ 02-ignite-timer/src/App.tsx
@@ -1,3 +1,17 @@
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+import { ThemeProvider } from 'styled-components'
+
 export function App() {
-  return <div>Hello World</div>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
