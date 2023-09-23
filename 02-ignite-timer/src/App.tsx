84d474a96889c01af69482ea33f33e89ab76aca4
EliasMedeirosDev <eliasmedeiros898@gmail.com>
2023-09-23 15:47:46 -0300

    Ajustes

--- 02-ignite-timer/src/App.tsx
+++ 02-ignite-timer/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter } from 'react-router-dom'
 import { Router } from './Router'
-
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 import { ThemeProvider } from 'styled-components'
