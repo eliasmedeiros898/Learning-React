d0115009df7a2a457d24969245f6949da4b75159
EliasMedeirosDev <102063068+eliasmedeiros898@users.noreply.github.com>
2024-03-06 21:06:03 -0300

    adicionada lógica inicial

--- 03- DTMoney/dtmoney/src/components/Summary/index.tsx
+++ 03- DTMoney/dtmoney/src/components/Summary/index.tsx
@@ -8,6 +8,26 @@ export function Summary() {
 
     const { transactions } = useContext(TransactionsContext)
 
+    const summary = transactions.reduce(
+        (acc, transaction) => {
+            if(transaction.type==='income'){
+                acc.income += transaction.price
+                acc.total += transaction.price
+            } else{
+                acc.outcome += transaction.price
+                acc.total -= transaction.price
+            }
+
+            return acc
+        }, 
+        {
+            income: 0, 
+            outcome: 0, 
+            total: 0
+        }
+    )
+    
+    
     
     return(
         <SummaryContainer>
@@ -17,7 +37,7 @@ export function Summary() {
                     <ArrowCircleUp size={32} color="#00b37e"/>
                 </header>
                 <strong>
-                    R$ 17.400,00
+                    {summary.income}
                 </strong>
             </SummaryCard>
             <SummaryCard>
@@ -26,7 +46,7 @@ export function Summary() {
                     <ArrowCircleDown size={32} color="#f75a68"/>
                 </header>
                 <strong>
-                    R$ 17.400,00
+                    {summary.outcome}
                 </strong>
             </SummaryCard>
             <SummaryCard variant="green">
@@ -35,7 +55,7 @@ export function Summary() {
                     <CurrencyDollar size={32} color="#fff"/>
                 </header>
                 <strong>
-                    R$ 17.400,00
+                    {summary.total}
                 </strong>
             </SummaryCard>
         </SummaryContainer>
--- 03- DTMoney/dtmoney/src/contexts/TransactionsContext.tsx
+++ 03- DTMoney/dtmoney/src/contexts/TransactionsContext.tsx
@@ -18,6 +18,8 @@ interface TransactionsProviderProps {
     children: ReactNode
 }
 
+
+
 export const TransactionsContext = createContext({} as TransactionContextType)
 
 export function TransactionsProvider({ children } : TransactionsProviderProps) {
